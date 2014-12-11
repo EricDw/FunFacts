d6eb758e6f16724a619961ae057e4e5aee75340e
EricDw <dewildte@gmail.com>
2014-12-11 04:02:28 -0500

    Created an array that holds random facts and a random number generator that generates numbers equal to the number of objects in the array. Programmed the button to display a random fact from the array of random facts on the screen.

--- app/src/main/java/com/publicmethod/eric/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/publicmethod/eric/funfacts/FunFactsActivity.java
@@ -13,6 +13,16 @@ import java.util.Random;
 
 public class FunFactsActivity extends ActionBarActivity {
 
+
+//                The empty quotes give the variable a value but its just a value of nothing.
+//                This gives the system something to assign preventing a crash
+
+//                Declare all of our class variables
+
+    String fact = "";
+    Random randomGenerator = new Random();
+    int randomNumber;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,21 +32,40 @@ public class FunFactsActivity extends ActionBarActivity {
         final TextView factLabel = (TextView) findViewById(R.id.factTextView);
         Button showFactButton = (Button) findViewById(R.id.showFactButton);
 
+
+        factLabel.setText(fact);
+
         showFactButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
 //                The button was clicked so update the fact label with a fact
-                String fact = "";
-//                The empty quotes give the variable a value but its just a value of nothing
-//                This gives the system something to assign even though its essentially nothing
-//                preventing a crash
+
+//                String[] statementsArray is a solution to an extra curricular assignment
+                String[] statementsArray = new  String[]{
+                        "Ants Stretch when they wake up in the morning.",
+                        "Ostriches can run faster than horses.",
+                        "Olympic gold medals are actually mostly silver.",
+                        "You are born with 300 bones; by the time you are an adult you will have 206.",
+                        "It takes about 8 minuets for light from the sun to reach earth.",
+                        "Some bamboo plants can grow almost a meter in just one day.",
+                        "The state of Florida is bigger than England.",
+                        "Some penguins can leap 2-3 meters out of the water.",
+                        "On average, it takes 66 days to form a new habit.",
+                        "Mammoths still walked the earth when the Great Pyramid was being built."
+                };
+
+
 
 //                Randomly select a fact
-                Random randomGenerator = new Random(); // Construct a new random number generator
-                int randomNumber = randomGenerator.nextInt(3);
+//                Construct a new random number generator
+                 randomNumber = randomGenerator.nextInt(statementsArray.length);
 
-                fact = randomNumber + "";// + can be used to not just to add numbers together but also
-                                         // to stick strings and ints together
+//                Convert the number to a text fact from the String[]
+
+                fact = statementsArray[randomNumber] ;/* + can be used not just to add numbers together but also
+                                                   * to stick strings and ints together
+                                                   */
 
 //                Update the label with our dynamic fact
                 factLabel.setText(fact);
