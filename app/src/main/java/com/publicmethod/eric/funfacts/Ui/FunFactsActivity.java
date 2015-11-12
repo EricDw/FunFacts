9730653440ad0f2f26003536eaaba776c7c8b510
Eric <dewildte@gmail.com>
2015-11-12 01:35:37 -0500

    Project now persists data for the purpose of orientation changes.

--- app/src/main/java/com/publicmethod/eric/funfacts/Ui/FunFactsActivity.java
+++ app/src/main/java/com/publicmethod/eric/funfacts/Ui/FunFactsActivity.java
@@ -2,7 +2,6 @@ package com.publicmethod.eric.funfacts.Ui;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -19,12 +18,20 @@ public class FunFactsActivity extends ActionBarActivity {
 //                The empty quotes give the variable a value but its just a value of nothing.
 //                This gives the system something to assign preventing a crash
 
-//                Declare all of our class variables
-    public  static  final String TAG = FunFactsActivity.class.getSimpleName();
+    //                Declare all of our class variables
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
+
+    private static final String KEY_COLOR = "KEY_COLOR";
+    private static final String KEY_FACT = "KEY_FACT";
 
     private FactBook mFactBook = new FactBook();
     private ColorWheel mColorWheel = new ColorWheel();
+    private int mColor;
+    private String mFact;
 
+    private TextView mFactLabel;
+    private Button mShowFactButton;
+    private RelativeLayout mRelativeLayout;
 
 
     @Override
@@ -33,12 +40,10 @@ public class FunFactsActivity extends ActionBarActivity {
         setContentView(R.layout.activity_fun_facts);
 
 //        Declare our view variables and assign the Views from the layout file
-        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
-        final Button showFactButton = (Button) findViewById(R.id.showFactButton);
-        final RelativeLayout mRelativeLayout = (RelativeLayout) findViewById(R.id.factLayout);
+        mFactLabel = (TextView) findViewById(R.id.factTextView);
+        mShowFactButton = (Button) findViewById(R.id.showFactButton);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.factLayout);
 
-
-        factLabel.setText("");
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
@@ -46,18 +51,45 @@ public class FunFactsActivity extends ActionBarActivity {
 
 //                Update the label with our dynamic fact from FactBook
 //                Do the same thing with the background color from ColorWheel
+                mFact = mFactBook.getFact();
+                mFactLabel.setText(mFact);
 
-                int color = mColorWheel.getColor();
-                String fact = mFactBook.getFact();
-                mRelativeLayout.setBackgroundColor(color);
-                factLabel.setText(fact);
-                showFactButton.setTextColor(color);
+                mColor = mColorWheel.getColor();
+                mRelativeLayout.setBackgroundColor(mColor);
+                mShowFactButton.setTextColor(mColor);
 
             }
         };
-        showFactButton.setOnClickListener(listener);
-        Log.d(TAG,"Yay our activity was created!");
-//        Toast.makeText(getApplicationContext(), "Yay our activity was created!", Toast.LENGTH_LONG).show();
+        mShowFactButton.setOnClickListener(listener);
+
+        if (mFactLabel.getText().toString().isEmpty()) {
+            mFact = mFactBook.getFirstFact();
+            mFactLabel.setText(mFact);
+            mColor = mColorWheel.getColorGreen();
+            mShowFactButton.setTextColor(mColor);
+            mRelativeLayout.setBackgroundColor(mColor);
+
+        }
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString(KEY_FACT, mFact);
+        outState.putInt(KEY_COLOR, mColor);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        mFact = savedInstanceState.getString(KEY_FACT);
+        mFactLabel.setText(mFact);
+        mColor = savedInstanceState.getInt(KEY_COLOR);
+        mShowFactButton.setTextColor(mColor);
+        mRelativeLayout.setBackgroundColor(mColor);
 
     }
 }
